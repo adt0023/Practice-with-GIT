e6e3fc5b30e310ca9c706907b2821baeca8fc374
adt0023 <adt0023@mix.wvu.edu>
2019-02-27 21:03:03 -0500

    added geom_hist

--- Git_practice.R
+++ Git_practice.R
@@ -2,3 +2,6 @@ a<-1:200
 cheesecake<-rep(1:10, 20)
 a_cheesecake<-data.frame(a,cheesecake)
 head(a_cheesecake)
+library(ggplot2)
+g1<- ggplot(a_cheesecake, aes(x=cheesecake))+geom_histogram()+geom_histogram(aes(x=a))
+g1
