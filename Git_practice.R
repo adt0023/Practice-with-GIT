5ec387da74f06069b726f4c256a5bc4295dbc979
adt0023 <adt0023@mix.wvu.edu>
2019-02-27 21:12:36 -0500

    added categorical and stuff

--- Git_practice.R
+++ Git_practice.R
@@ -5,3 +5,6 @@ head(a_cheesecake)
 library(ggplot2)
 g1<- ggplot(a_cheesecake, aes(x=cheesecake,col="red"))+geom_histogram()+geom_histogram(aes(x=a, col="blue"))
 g1
+a_cheesecake$what<-rep(c("yes","no"),100)
+g2<-ggplot(a_cheesecake, aes(x=what, y=cheesecake))+geom_boxplot()+geom_point(aes(col=a))+geom_jitter()
+g2
