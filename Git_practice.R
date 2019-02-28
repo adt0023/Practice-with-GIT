c876507a3811577c0e1ac50a7a16d5322b6b1402
adt0023 <adt0023@mix.wvu.edu>
2019-02-27 21:11:36 -0500

    added categorical and geom_boxplot with jitter

--- Git_practice.R
+++ Git_practice.R
@@ -3,5 +3,5 @@ cheesecake<-rep(1:10, 20)
 a_cheesecake<-data.frame(a,cheesecake)
 head(a_cheesecake)
 library(ggplot2)
-g1<- ggplot(a_cheesecake, aes(x=cheesecake))+geom_histogram()+geom_histogram(aes(x=a))
+g1<- ggplot(a_cheesecake, aes(x=cheesecake,col="red"))+geom_histogram()+geom_histogram(aes(x=a, col="blue"))
 g1
