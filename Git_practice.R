cf41467abf3fa4e8e4bb11531fcb3cbd2bede272
adt0023 <adt0023@mix.wvu.edu>
2019-03-06 16:48:44 -0500

    added bananagrams and applegram variables, added to a_cheesecake dataframe

--- Git_practice.R
+++ Git_practice.R
@@ -17,3 +17,9 @@ g5<-ggplot(a_cheesecake, aes(x=cheesecake))+geom_histogram(aes(col=what))+geom_h
 g5
 pie<-3.14*c(a_cheesecake$cheesecake,a_cheesecake$bananabread)
 a_cheesecake$pie<-head(sqrt(pie),200)
+a_cheesecake$pie<-head(sqrt(pie),200)
+
+bananagrams<- rep(c("A","E"),200)
+
+applegrams<-rep(c("alpha","beta"),100)
+a_cheesecake$applegrams<-applegrams
